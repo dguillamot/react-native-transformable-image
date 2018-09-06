275ea2d06cf956594590f9056b5e1bd6d9a7faf9
Daniel Guillamot <dguillamot@gmail.com>
2018-09-06 15:23:27 +0900

    custom react-native-view-transformation

--- library/TransformableImage.js
+++ library/TransformableImage.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-import React, { Component, PropTypes } from 'react';
-import { Image } from 'react-native';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Image } from "react-native";
 
-import ViewTransformer from 'react-native-view-transformer';
+import ViewTransformer from "react-native-view-transformer";
 
 let DEV = false;
 
 export default class TransformableImage extends Component {
-
   static enableDebug() {
     DEV = true;
   }
@@ -16,7 +16,7 @@ export default class TransformableImage extends Component {
   static propTypes = {
     pixels: PropTypes.shape({
       width: PropTypes.number,
-      height: PropTypes.number,
+      height: PropTypes.number
     }),
 
     enableTransform: PropTypes.bool,
@@ -55,7 +55,10 @@ export default class TransformableImage extends Component {
   componentWillReceiveProps(nextProps) {
     if (!sameSource(this.props.source, nextProps.source)) {
       //image source changed, clear last image's pixels info if any
-      this.setState({pixels: undefined, keyAcumulator: this.state.keyAcumulator + 1})
+      this.setState({
+        pixels: undefined,
+        keyAcumulator: this.state.keyAcumulator + 1
+      });
       this.getImageSize(nextProps.source);
     }
   }
@@ -78,16 +81,18 @@ export default class TransformableImage extends Component {
     if (width && height) {
       contentAspectRatio = width / height;
       if (this.state.width && this.state.height) {
-        maxScale = Math.max(width / this.state.width, height / this.state.height);
+        maxScale = Math.max(
+          width / this.state.width,
+          height / this.state.height
+        );
         maxScale = Math.max(1, maxScale);
       }
     }
 
-
     return (
       <ViewTransformer
-        ref='viewTransformer'
-        key={'viewTransformer#' + this.state.keyAccumulator} //when image source changes, we should use a different node to avoid reusing previous transform state
+        ref="viewTransformer"
+        key={"viewTransformer#" + this.state.keyAccumulator} //when image source changes, we should use a different node to avoid reusing previous transform state
         enableTransform={this.props.enableTransform && this.state.imageLoaded} //disable transform until image is loaded
         enableScale={this.props.enableScale}
         enableTranslate={this.props.enableTranslate}
@@ -98,14 +103,15 @@ export default class TransformableImage extends Component {
         maxScale={maxScale}
         contentAspectRatio={contentAspectRatio}
         onLayout={this.onLayout.bind(this)}
-        style={this.props.style}>
+        style={this.props.style}
+      >
         <Image
           {...this.props}
-          style={[this.props.style, {backgroundColor: 'transparent'}]}
-          resizeMode={'contain'}
+          style={[this.props.style, { backgroundColor: "transparent" }]}
+          resizeMode={"contain"}
           onLoadStart={this.onLoadStart.bind(this)}
           onLoad={this.onLoad.bind(this)}
-          capInsets={{left: 0.1, top: 0.1, right: 0.1, bottom: 0.1}} //on iOS, use capInsets to avoid image downsampling
+          capInsets={{ left: 0.1, top: 0.1, right: 0.1, bottom: 0.1 }} //on iOS, use capInsets to avoid image downsampling
         />
       </ViewTransformer>
     );
@@ -126,7 +132,7 @@ export default class TransformableImage extends Component {
   }
 
   onLayout(e) {
-    let {width, height} = e.nativeEvent.layout;
+    let { width, height } = e.nativeEvent.layout;
     if (this.state.width !== width || this.state.height !== height) {
       this.setState({
         width: width,
@@ -136,37 +142,54 @@ export default class TransformableImage extends Component {
   }
 
   getImageSize(source) {
-    if(!source) return;
+    if (!source) return;
 
-    DEV && console.log('getImageSize...' + JSON.stringify(source));
+    DEV && console.log("getImageSize..." + JSON.stringify(source));
 
-    if (typeof Image.getSize === 'function') {
+    if (typeof Image.getSize === "function") {
       if (source && source.uri) {
         Image.getSize(
           source.uri,
           (width, height) => {
-            DEV && console.log('getImageSize...width=' + width + ', height=' + height);
+            DEV &&
+              console.log(
+                "getImageSize...width=" + width + ", height=" + height
+              );
             if (width && height) {
-              if(this.state.pixels && this.state.pixels.width === width && this.state.pixels.height === height) {
+              if (
+                this.state.pixels &&
+                this.state.pixels.width === width &&
+                this.state.pixels.height === height
+              ) {
                 //no need to update state
               } else {
-                this.setState({pixels: {width, height}});
+                this.setState({ pixels: { width, height } });
               }
             }
           },
-          (error) => {
-            console.error('getImageSize...error=' + JSON.stringify(error) + ', source=' + JSON.stringify(source));
-          })
+          error => {
+            console.error(
+              "getImageSize...error=" +
+                JSON.stringify(error) +
+                ", source=" +
+                JSON.stringify(source)
+            );
+          }
+        );
       } else {
-        console.warn('getImageSize...please provide pixels prop for local images');
+        console.warn(
+          "getImageSize...please provide pixels prop for local images"
+        );
       }
     } else {
-      console.warn('getImageSize...Image.getSize function not available before react-native v0.28');
+      console.warn(
+        "getImageSize...Image.getSize function not available before react-native v0.28"
+      );
     }
   }
 
   getViewTransformerInstance() {
-    return this.refs['viewTransformer'];
+    return this.refs["viewTransformer"];
   }
 }
 
